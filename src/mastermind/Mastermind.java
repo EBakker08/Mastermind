c41422a0425233a1540f58c1a39003d8a0ce4a27
EBakker08 <1218556@student.roc-nijmegen.nl>
2025-10-08 15:33:47 +0200

    EoD
    --:-- 08/10/2025
    'Statement' achter wine en lose gezet.
    input variablen versimpeld.

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -26,12 +26,12 @@ public class Mastermind {
 			String codeVakje2 = orange;
 			String codeVakje3 = purple;
 			String codeVakje4 = yellow;
-			String x1 = red;
-			String x2 = green;
+//			String x1 = red;
+//			String x2 = green;
 			
 			//win/lose statements
-			String win = "Congratulations, you won!";
-			String lose = "10 rounds are over. You lose.";
+			String winStatement = "Congratulations, you won!";
+			String loseStatement = "10 rounds are over. You lose.";
 			
 			System.out.println("Welcome to Mastermind!\nIn this game you have to guess the right code."); //intro van de game
 
@@ -43,108 +43,88 @@ public class Mastermind {
 				System.out.println("Round: " + round + "/10.\nYou can choose between Red, Blue, Yellow, Green, Purple and Orange.");
 				
 				//user input
-				String input1_1 = mastermind.next();
-				String input1_2 = mastermind.next();
-				String input1_3 = mastermind.next();
-				String input1_4 = mastermind.next();
+				String input1 = mastermind.next();
+				String input2 = mastermind.next();
+				String input3 = mastermind.next();
+				String input4 = mastermind.next();
 				
 				//input 1
-				if (input1_1.equalsIgnoreCase(codeVakje1)) {
+				if (input1.equalsIgnoreCase(codeVakje1)) {
 					System.out.println(black);
 				} else {
-					if (input1_1.equalsIgnoreCase(codeVakje2)) {
+					if (input1.equalsIgnoreCase(codeVakje2)) {
 						System.out.println(white);
 					}
-					if (input1_1.equalsIgnoreCase(codeVakje3)) {
+					if (input1.equalsIgnoreCase(codeVakje3)) {
 						System.out.println(white);
 					}
-					if (input1_1.equalsIgnoreCase(codeVakje4)) {
+					if (input1.equalsIgnoreCase(codeVakje4)) {
 						System.out.println(white);
 					} else {
-						if (input1_1.equalsIgnoreCase(x1)) {
-							System.out.println(x);
-						}
-						if (input1_1.equalsIgnoreCase(x2)) {
-							System.out.println(x);
-						}
+						System.out.println(x);
 					}
 
 				}
 				
 				//Input 2
-				if (input1_2.equalsIgnoreCase(codeVakje2)) {
+				if (input2.equalsIgnoreCase(codeVakje2)) {
 					System.out.println(black);
 				} else {
-					if (input1_2.equalsIgnoreCase(codeVakje1)) {
+					if (input2.equalsIgnoreCase(codeVakje1)) {
 						System.out.println(white);
 					}
-					if (input1_2.equalsIgnoreCase(codeVakje3)) {
+					if (input2.equalsIgnoreCase(codeVakje3)) {
 						System.out.println(white);
 					}
-					if (input1_2.equalsIgnoreCase(codeVakje4)) {
+					if (input2.equalsIgnoreCase(codeVakje4)) {
 						System.out.println(white);
 					} else {
-						if (input1_2.equalsIgnoreCase(x1)) {
-							System.out.println(x);
-						}
-						if (input1_2.equalsIgnoreCase(x2)) {
-							System.out.println(x);
-						}
+						System.out.println(x);
 					}
 
 				}
 				
 				//Input 3
-				if (input1_3.equalsIgnoreCase(codeVakje3)) {
+				if (input3.equalsIgnoreCase(codeVakje3)) {
 					System.out.println(black);
 				} else {
-					if (input1_3.equalsIgnoreCase(codeVakje1)) {
+					if (input3.equalsIgnoreCase(codeVakje1)) {
 						System.out.println(white);
 					}
-					if (input1_3.equalsIgnoreCase(codeVakje2)) {
+					if (input3.equalsIgnoreCase(codeVakje2)) {
 						System.out.println(white);
 					}
-					if (input1_3.equalsIgnoreCase(codeVakje4)) {
+					if (input3.equalsIgnoreCase(codeVakje4)) {
 						System.out.println(white);
 					} else {
-						if (input1_3.equalsIgnoreCase(x1)) {
-							System.out.println(x);
-						}
-						if (input1_3.equalsIgnoreCase(x2)) {
-							System.out.println(x);
-						}
+						System.out.println(x);
 					}
 
 				}
 
 				//Input 4
-				if (input1_4.equalsIgnoreCase(codeVakje4)) {
+				if (input4.equalsIgnoreCase(codeVakje4)) {
 					System.out.println(black);
 				} else {
-					if (input1_4.equalsIgnoreCase(codeVakje1)) {
+					if (input4.equalsIgnoreCase(codeVakje1)) {
 						System.out.println(white);
 					}
-					if (input1_4.equalsIgnoreCase(codeVakje2)) {
+					if (input4.equalsIgnoreCase(codeVakje2)) {
 						System.out.println(white);
 					}
-					if (input1_4.equalsIgnoreCase(codeVakje3)) {
+					if (input4.equalsIgnoreCase(codeVakje3)) {
 						System.out.println(white);
 					} else {
-						if (input1_4.equalsIgnoreCase(x1)) {
-							System.out.println(x);
-						}
-						if (input1_4.equalsIgnoreCase(x2)) {
-							System.out.println(x);
-						}
+						System.out.println(x);
 					}
 
 				}
 				
 				//controlle gebied
-				if (input1_1.equalsIgnoreCase(codeVakje1)) {
-					if (input1_2.equalsIgnoreCase(codeVakje2)) {
-						if (input1_3.equalsIgnoreCase(codeVakje3)) {
-							if (input1_4.equalsIgnoreCase(codeVakje4)) {
+				if (input1.equalsIgnoreCase(codeVakje1)) {
+					if (input2.equalsIgnoreCase(codeVakje2)) {
+						if (input3.equalsIgnoreCase(codeVakje3)) {
+							if (input4.equalsIgnoreCase(codeVakje4)) {
 								round = 12;
 							} else {						//else = next round
 								round = round + 1;
@@ -163,9 +143,9 @@ public class Mastermind {
 			//win/lose conditions
 			while (round < 11);
 			if (round == 11) {
-				System.out.println(lose);
+				System.out.println(loseStatement);
 			} else {
-				System.out.println(win);
+				System.out.println(winStatement);
 			}
 			
 			
