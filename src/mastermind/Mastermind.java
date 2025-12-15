d6f4d67e6835ad3eaf5490a250a8779ab8545965
EBakker08 <1218556@student.roc-nijmegen.nl>
2025-12-15 13:06:47 +0100

    EoD
    13:08 15/12/2025

--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,3 +1,4 @@
+
 package mastermind;
 
 import java.util.Random;
@@ -22,8 +23,8 @@ public class Mastermind {
 		String[] codeVakjes =
 				{"codeVakje1", "codeVakje2", "codeVakje3", "codeVakje4"};
 		
-		for (int i = 0; i < codeVakjes.length; i++) {
-		    codeVakjes[i] = colors[randomCode.nextInt(colors.length)];
+		for (int length = 0; length < codeVakjes.length; length++) {
+		    codeVakjes[length] = colors[randomCode.nextInt(colors.length)];
 		}
 		
 //		Win/lose statements
@@ -32,7 +33,7 @@ public class Mastermind {
 
 //		Introduction/welcome statement
 		System.out.println("Welcome to Mastermind!\nIn this game you have to guess the right code.\n(The code can include multiple of the same colors.)");
-
+		
 //		-----------------------------------------------------------------------------------------------------------------------------
 		
 //		Round variable
@@ -42,91 +43,34 @@ public class Mastermind {
 		do {
 //			Uitleg
 			System.out.println("Round: " + round + "/10.\nYou can choose between Red, Blue, Yellow, Green, Orange and Purple.");
-
+			
 //			User input variablen
 			String[] userInput =
-				{"input1", "input2", "input3", "input4"};
+				{"1", "2", "3", "4"};
 			
-			userInput[0] = mastermind.next();
-			userInput[1] = mastermind.next();
-			userInput[2] = mastermind.next();
-			userInput[3] = mastermind.next();
-
-//			Input 1 check
-			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[0].equalsIgnoreCase(codeVakjes[1])) {
-					System.out.println(checkColors[1]);
+//			
+			for (int i = 0; i < userInput.length; i++) {
+				userInput[i] = mastermind.next();
+			}
+			
+			boolean check = true;
+			
+//			
+			for (int i = 0; i < userInput.length; i++) {
+				if (userInput[i].equalsIgnoreCase(codeVakjes[i])) {
+					System.out.println(checkColors[0]);
+					check = false;
 				} else {
-					if (userInput[0].equalsIgnoreCase(codeVakjes[2])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[0].equalsIgnoreCase(codeVakjes[3])) {
+					for (int y = 0; y < codeVakjes.length; y++) {
+						if (userInput[i].equalsIgnoreCase(codeVakjes[y])) {
 							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
+							check = false;
+						} 
+					}if (check) {
+						System.out.println(checkColors[2]);
 						}
-					}
 				}
-			}	//End input 1
-
-//			Input 2 check
-			if (userInput[1].equalsIgnoreCase(codeVakjes[1])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[1].equalsIgnoreCase(codeVakjes[0])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[1].equalsIgnoreCase(codeVakjes[2])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[1].equalsIgnoreCase(codeVakjes[3])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
-					}
-				}
-			}	//end input 2
-
-//			Input 3 check
-			if (userInput[2].equalsIgnoreCase(codeVakjes[2])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[2].equalsIgnoreCase(codeVakjes[0])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[2].equalsIgnoreCase(codeVakjes[1])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[2].equalsIgnoreCase(codeVakjes[3])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
-					}
-				}
-			}	//End input 3
-
-//			Input 4 check
-			if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[3].equalsIgnoreCase(codeVakjes[0])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[3].equalsIgnoreCase(codeVakjes[1])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[3].equalsIgnoreCase(codeVakjes[2])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
-					}
-				}
-			}	//End input 4
+			}
 
 //			Game won check:
 			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
@@ -154,7 +98,9 @@ public class Mastermind {
 			System.out.println(loseStatement + "\n");
 			System.out.println("The code was:\n" + codeVakjes[0] + "\n" + codeVakjes[1] + "\n" + codeVakjes[2] + "\n" + codeVakjes[3]);
 		} else {
-			System.out.println(winStatement);
+			if (round == 12) {
+				System.out.println(winStatement);
+			}
 		}
 //		----------------------------------------------------------------------------------------------------------------------------
 		
