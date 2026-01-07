9aa7b5330d02f35144c12076e58b11467fd594e5
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-01-07 11:07:16 +0100

    EoD
    11:07 07/01/2026

--- src/mastermind/Helper.java
+++ src/mastermind/Helper.java
@@ -39,28 +39,107 @@ public class Helper {
 		do {
 			System.out.println("Round: " + round + "/10.\nYou can choose between Red, Blue, Yellow, Green, Orange and Purple.");
 			
-			String[] userInput =
-				{"1", "2", "3", "4"};
-			
+			String[] userInput = {"1", "2", "3", "4"};
+
 			for (int i = 0; i < userInput.length; i++) {
-				userInput[i] = sc.next();
+			    boolean validInput = false;
+			    while (!validInput) {	// keep asking for input until a valid color is entered
+			        try {
+//			        	read the user's input for this slot
+			            userInput[i] = sc.next();
+
+			            // check if the input matches any of the allowed colors
+			            for (String color : colors) {
+			                if (userInput[i].equalsIgnoreCase(color)) {
+			                    validInput = true;
+			                    break; // no need to check other colors
+			                }
+			            }
+
+			            if (!validInput) { // if input is not valid, try again
+			                System.out.println("Invalid color. Please enter one of: Red, Blue, Green, Yellow, Orange, Purple.");
+			            }
+
+			        } catch (Exception e) { // If a unexpected error occurs
+			            System.out.println("Invalid input, please try again.");
+			            sc.next(); // eat the invalid input so the scanner doesn't get stuck
+			        }
+			    }
 			}
 			
-			boolean check = true;
-			
-			for (int i = 0; i < userInput.length; i++) {
-				if (userInput[i].equalsIgnoreCase(codeVakjes[i])) {
-					System.out.println(checkColors[0]);
-					check = false;
+//			input 1 check
+			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
+				System.out.println(checkColors[0]);
+			} else {
+				if (userInput[0].equalsIgnoreCase(codeVakjes[1])) {
+					System.out.println(checkColors[1]);
 				} else {
-					for (int y = 0; y < codeVakjes.length; y++) {
-						if (userInput[i].equalsIgnoreCase(codeVakjes[y])) {
+					if (userInput[0].equalsIgnoreCase(codeVakjes[2])) {
+						System.out.println(checkColors[1]);
+					} else {
+						if (userInput[0].equalsIgnoreCase(codeVakjes[3])) {
 							System.out.println(checkColors[1]);
-							check = false;
-						} 
-					}if (check) {
-						System.out.println(checkColors[2]);
+						} else {
+							System.out.println(checkColors[2]);
 						}
+					}
+				}
+			}
+			
+//			input 2 check
+			if (userInput[1].equalsIgnoreCase(codeVakjes[1])) {
+				System.out.println(checkColors[0]);
+			} else {
+				if (userInput[1].equalsIgnoreCase(codeVakjes[0])) {
+					System.out.println(checkColors[1]);
+				} else {
+					if (userInput[1].equalsIgnoreCase(codeVakjes[2])) {
+						System.out.println(checkColors[1]);
+					} else {
+						if (userInput[1].equalsIgnoreCase(codeVakjes[3])) {
+							System.out.println(checkColors[1]);
+						} else {
+							System.out.println(checkColors[2]);
+						}
+					}
+				}
+			}
+			
+//			input 3 check
+			if (userInput[2].equalsIgnoreCase(codeVakjes[2])) {
+				System.out.println(checkColors[0]);
+			} else {
+				if (userInput[2].equalsIgnoreCase(codeVakjes[0])) {
+					System.out.println(checkColors[1]);
+				} else {
+					if (userInput[2].equalsIgnoreCase(codeVakjes[1])) {
+						System.out.println(checkColors[1]);
+					} else {
+						if (userInput[2].equalsIgnoreCase(codeVakjes[3])) {
+							System.out.println(checkColors[1]);
+						} else {
+							System.out.println(checkColors[2]);
+						}
+					}
+				}
+			}
+			
+//			input 4 check
+			if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {
+				System.out.println(checkColors[0]);
+			} else {
+				if (userInput[3].equalsIgnoreCase(codeVakjes[0])) {
+					System.out.println(checkColors[1]);
+				} else {
+					if (userInput[3].equalsIgnoreCase(codeVakjes[1])) {
+						System.out.println(checkColors[1]);
+					} else {
+						if (userInput[3].equalsIgnoreCase(codeVakjes[2])) {
+							System.out.println(checkColors[1]);
+						} else {
+							System.out.println(checkColors[2]);
+						}
+					}
 				}
 			}
 			
