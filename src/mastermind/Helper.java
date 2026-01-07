0a54d18a13f2917a98c247d29b6ba4a942ce7b25
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-01-07 10:38:37 +0100

    EoD
    10:38 07/01/2026

--- src/mastermind/Helper.java
+++ src/mastermind/Helper.java
@@ -1,24 +1,96 @@
 
 package mastermind;
 
+import java.util.Scanner;
 import java.util.Random;
 
 public class Helper {
 	
+	Scanner sc = new Scanner(System.in);
 	Random randomCode = new Random();
 	
 	String[] colors =
 		{"red", "blue", "green", "yellow", "orange", "purple"};
 	
-	public String[] codeMaken() {
-		String[] codeVakjes =
-			{"codeVakje1", "codeVakje2", "codeVakje3", "codeVakje4"};
-		
+	String[] codeVakjes =
+		{"codeVakje1", "codeVakje2", "codeVakje3", "codeVakje4"};
+	
+	String[] checkColors =
+		{"black", "white", "x"};
+	
+	String winStatement = "Congratulations, you won!";
+	String loseStatement = "10 rounds are over. You lose.";
+	
+//	Begin functions
+//	Code generation
+	public String[] codeMaking() {
 		for (int length = 0; length < codeVakjes.length; length++) {
 		    codeVakjes[length] = colors[randomCode.nextInt(colors.length)];
 		}
-		
 		return codeVakjes;
 	}
 	
+//	--------------------------------------------------------------------------------------------------------------------------------------------
+	
+	public int userInput() {
+		
+		int round = 1;
+		
+		do {
+			System.out.println("Round: " + round + "/10.\nYou can choose between Red, Blue, Yellow, Green, Orange and Purple.");
+			
+			String[] userInput =
+				{"1", "2", "3", "4"};
+			
+			for (int i = 0; i < userInput.length; i++) {
+				userInput[i] = sc.next();
+			}
+			
+			boolean check = true;
+			
+			for (int i = 0; i < userInput.length; i++) {
+				if (userInput[i].equalsIgnoreCase(codeVakjes[i])) {
+					System.out.println(checkColors[0]);
+					check = false;
+				} else {
+					for (int y = 0; y < codeVakjes.length; y++) {
+						if (userInput[i].equalsIgnoreCase(codeVakjes[y])) {
+							System.out.println(checkColors[1]);
+							check = false;
+						} 
+					}if (check) {
+						System.out.println(checkColors[2]);
+						}
+				}
+			}
+			
+			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
+				if (userInput[1].equalsIgnoreCase(codeVakjes[1])) {
+					if (userInput[2].equalsIgnoreCase(codeVakjes[2])) {
+						if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {
+							round = 12;
+						} else {	// Else = next round
+							round++;
+						}
+					} else {
+						round++;
+					}
+				} else {
+					round++;
+				}
+			} else {
+				round++;
+			}
+			
+			if (round == 11) {
+				System.out.println(loseStatement + "\n");
+				System.out.println("The code was:\n" + codeVakjes[0] + "\n" + codeVakjes[1] + "\n" + codeVakjes[2] + "\n" + codeVakjes[3]);
+			} else {
+				if (round == 12) {
+					System.out.println(winStatement);
+				}
+			}
+		} while (round < 11);
+		return round;
+	}
 }
--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -1,99 +1,19 @@
 
 package mastermind;
 
-import java.util.Scanner;
-
 public class Mastermind {
 
 	public static void main(String[] args) {
-		Scanner mastermind = new Scanner(System.in);	//Start scanner
 		Helper helper = new Helper();	//Start of functions	(Helper.java)
 		
-//		---------------------------------------------------------------------------------------------------------------------------
+//		Code generation
+		helper.codeMaking();
 		
-//		Check colors:
-		String[] checkColors =
-			{"black", "white", "x"};
-		
-		String[] codeVakjes = helper.codeMaken();	//Making random code
-		
-//		Win/lose statements
-		String winStatement = "Congratulations, you won!";
-		String loseStatement = "10 rounds are over. You lose.";
-
 //		Introduction/welcome statement
 		System.out.println("Welcome to Mastermind!\nIn this game you have to guess the right code.\n(The code can include multiple of the same colors.)");
 		
-//		-----------------------------------------------------------------------------------------------------------------------------
+//		User input and game won check
+		helper.userInput();
 		
-//		Round variable
-		int round = 1;
-		
-//		The game (check) loop
-		do {
-//			Uitleg
-			System.out.println("Round: " + round + "/10.\nYou can choose between Red, Blue, Yellow, Green, Orange and Purple.");
-			
-//			User input variablen
-			String[] userInput =
-				{"1", "2", "3", "4"};
-			
-//			
-			for (int i = 0; i < userInput.length; i++) {
-				userInput[i] = mastermind.next();
-			}
-			
-			boolean check = true;
-			
-//			
-			for (int i = 0; i < userInput.length; i++) {
-				if (userInput[i].equalsIgnoreCase(codeVakjes[i])) {
-					System.out.println(checkColors[0]);
-					check = false;
-				} else {
-					for (int y = 0; y < codeVakjes.length; y++) {
-						if (userInput[i].equalsIgnoreCase(codeVakjes[y])) {
-							System.out.println(checkColors[1]);
-							check = false;
-						} 
-					}if (check) {
-						System.out.println(checkColors[2]);
-						}
-				}
-			}
-
-//			Game won check:
-			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
-				if (userInput[1].equalsIgnoreCase(codeVakjes[1])) {
-					if (userInput[2].equalsIgnoreCase(codeVakjes[2])) {
-						if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {
-							round = 12;
-						} else {	// Else = next round
-							round++;
-						}
-					} else {
-						round++;
-					}
-				} else {
-					round++;
-				}
-			} else {
-				round++;
-			}	//	End of game won check
-		}	//	End of do loop
-
-//		Win/lose conditions
-		while (round < 11);
-		if (round == 11) {
-			System.out.println(loseStatement + "\n");
-			System.out.println("The code was:\n" + codeVakjes[0] + "\n" + codeVakjes[1] + "\n" + codeVakjes[2] + "\n" + codeVakjes[3]);
-		} else {
-			if (round == 12) {
-				System.out.println(winStatement);
-			}
-		}
-//		----------------------------------------------------------------------------------------------------------------------------
-		
-		mastermind.close(); //End scanner
 	}
-}	//End program
+}	//	End program
