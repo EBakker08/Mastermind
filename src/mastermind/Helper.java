e68695ef0b0fb8d67325e725a2284900ea5aae22
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-01-16 23:28:17 +0100

    EoD

--- src/mastermind/Helper.java
+++ src/mastermind/Helper.java
@@ -6,6 +6,18 @@ import java.util.Random;
 
 public class Helper {
 	
+//	Deelproblemen:
+//	Generating random code
+//	userInput 4x
+//	If user puts anything but a color in, try again
+//	userInput checker
+//	Game won checker
+//	Printing of winStatement
+//	Printing of loseStatement
+//	Plays for 10 rounds
+	
+//	---------------------------------------------------------------------------------------------------------------
+	
 //	Key variables
 	Scanner sc = new Scanner(System.in);
 	Random randomCode = new Random();
@@ -26,6 +38,8 @@ public class Helper {
 	String winStatement = "Congratulations, you won!";
 	String loseStatement = "10 rounds are over. You lose.";
 	
+//	-----------------------------------------------------------------------------------------------------------------------------
+	
 //	Begin functions
 //	Random code generation
 	public String[] codeMaking() {
@@ -38,16 +52,18 @@ public class Helper {
 	
 //	--------------------------------------------------------------------------------------------------------------------------------------------
 	
-	public int userInput() {
+	public int userInputAndCodeCheck() {
 		
 		int round = 1;
 		
 		do {
-			System.out.println("Round: " + round + "/10.");
-			
 			String[] userInput = {"1", "2", "3", "4"};
+			
+			System.out.println("round: " + round + "/10");
 
-			for (int i = 0; i < userInput.length; i++) {	//	Loop user input until 4 inputs are received
+//			Deelprobleem:
+//			Input validator
+			for (int i = 0; i < userInput.length; i++) {	//	Loop user input until 4 inputs are given
 			    boolean validInput = false;	//	user input is false by default
 			    while (!validInput) {	//	Keep asking for input until a valid color is entered
 			        try {
@@ -65,95 +81,44 @@ public class Helper {
 			                System.out.println("Invalid color. Please enter: Red, Blue, Green, Yellow, Orange, Purple.");
 			            }
 
-			        } catch (Exception e) { //	If a unexpected error occurs
-			            System.out.println("Invalid input, please try again.");
-			            sc.next(); //	Eat the invalid input so the scanner doesn't get stuck
+			        } catch (Exception e) { //	If input is not valid, try again
+			            System.out.println("Invalid color. Please enter: Red, Blue, Green, Yellow, Orange, Purple.");
 			        }
 			    }
 			}
 			
-//			Input 1 check
-			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[0].equalsIgnoreCase(codeVakjes[1])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[0].equalsIgnoreCase(codeVakjes[2])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[0].equalsIgnoreCase(codeVakjes[3])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
+//			Deelprobleem:
+//			User Input checker
+//			Check code for as long as userInput is
+			for (int i = 0; i < userInput.length; i++) {
+				if (userInput[i].equalsIgnoreCase(codeVakjes[i])) {	//	If userInput = codeVakje then sysout black
+					System.out.println(checkColors[0]);
+					continue;	//	If color is not in right position, continue to next stuff
+				}
+				
+				boolean colorOnOtherPosition = false;
+				
+				for (int y = 0; y < codeVakjes.length; y++) {
+					if (y != i && userInput[i].equalsIgnoreCase(codeVakjes[y])) {	//	If codeVakje doesn't equal (!=) userInput and if color is found somewhere else then print white
+						colorOnOtherPosition = true;
+						break;
 					}
+				}
+				
+				if (colorOnOtherPosition == true) {
+					System.out.println(checkColors[1]);	//	sysout white, because color is found in codeVakjes but not in right position
+				} else {
+					System.out.println(checkColors[2]);	//	sysout x, because color is not found in any of codeVakjes
 				}
 			}
 			
-//			Input 2 check
-			if (userInput[1].equalsIgnoreCase(codeVakjes[1])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[1].equalsIgnoreCase(codeVakjes[0])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[1].equalsIgnoreCase(codeVakjes[2])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[1].equalsIgnoreCase(codeVakjes[3])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
-					}
-				}
-			}
-			
-//			Input 3 check
-			if (userInput[2].equalsIgnoreCase(codeVakjes[2])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[2].equalsIgnoreCase(codeVakjes[0])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[2].equalsIgnoreCase(codeVakjes[1])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[2].equalsIgnoreCase(codeVakjes[3])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
-					}
-				}
-			}
-			
-//			Input 4 check
-			if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {
-				System.out.println(checkColors[0]);
-			} else {
-				if (userInput[3].equalsIgnoreCase(codeVakjes[0])) {
-					System.out.println(checkColors[1]);
-				} else {
-					if (userInput[3].equalsIgnoreCase(codeVakjes[1])) {
-						System.out.println(checkColors[1]);
-					} else {
-						if (userInput[3].equalsIgnoreCase(codeVakjes[2])) {
-							System.out.println(checkColors[1]);
-						} else {
-							System.out.println(checkColors[2]);
-						}
-					}
-				}
-			}
-
-//			
+//			Deelprobleem:
+//			Game won check
 			if (userInput[0].equalsIgnoreCase(codeVakjes[0])) {
 				if (userInput[1].equalsIgnoreCase(codeVakjes[1])) {
 					if (userInput[2].equalsIgnoreCase(codeVakjes[2])) {
-						if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {
-							round = 12;
+						if (userInput[3].equalsIgnoreCase(codeVakjes[3])) {	//	Does every userInput equal the color in codeVakjes? If so end game, if not go  to next round
+							round = 12;	//	You won? Change round to 12, stopping the loop and printing winStatement
 						} else {	// Else = next round
 							round++;
 						}
@@ -166,16 +131,17 @@ public class Helper {
 			} else {
 				round++;
 			}
-			
-			if (round == 11) {
+//			Deelproblemen:
+			if (round == 11) {	//	If 10 round are over (1+10=11) then sysout loseStatement
 				System.out.println(loseStatement + "\n");
 				System.out.println("The code was:\n" + codeVakjes[0] + "\n" + codeVakjes[1] + "\n" + codeVakjes[2] + "\n" + codeVakjes[3]);
 			} else {
-				if (round == 12) {
+				if (round == 12) {	//	If round = 12 then sysout winStatement
 					System.out.println(winStatement);
 				}
 			}
-		} while (round < 11);
+		} while (round < 11);	//	Loop until 10 rounds are over
+		
 		return round;
 	}
 }
--- src/mastermind/Mastermind.java
+++ src/mastermind/Mastermind.java
@@ -10,10 +10,9 @@ public class Mastermind {
 		helper.codeMaking();
 		
 //		Introduction/welcome statement
-		System.out.println("Welcome to Mastermind!\nIn this game you have to guess the right code.\n(The code can include multiple of the same colors.)\nYou can choose between Red, Blue, Yellow, Green, Orange and Purple.");
+		System.out.println("Welcome to Mastermind!\nIn this game you have to guess the right code. (The code can include multiple of the same colors.)\nYou can choose between Red, Blue, Yellow, Green, Orange and Purple.");
 		
 //		User input, input check and game won check
-		helper.userInput();
-		
+		helper.userInputAndCodeCheck();
 	}
-}	//	End program
+}
